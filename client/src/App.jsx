8617b3d8210c85aacf6b02540ea3e21ad065f64f
Your GitHub Username <Your GitHub Email>
2025-03-03 11:12:27 +0530

    server dotenv issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {button} from "./components/button1"
+import {Button} from "./components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='min-h-screen bg-black'>
-      sjhbfj
-    </div>
-    <div className='min-l-screen font-extrabold bg-amber-200 '>
-      <button></button>
-    </div>
-
+    <Button className='bg-red-600'>Check</Button>
     </>
   )
 }
--- client/src/components/button1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const button = () => {
-  return (
-   <></>
-  )
-}
-
-export default button
