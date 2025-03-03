02ebd4d577ae823383768d6a5cbde74c866e7a2e
Your GitHub Username <Your GitHub Email>
2025-03-03 09:16:13 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {button} from "./components/button1"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='min-h-screen bg-black'>
+      sjhbfj
+    </div>
+    <div className='min-l-screen font-extrabold bg-amber-200 '>
+      <button></button>
+    </div>
+
+    </>
+  )
+}
+
+export default App
