9ddd82bb8d0bacfb930273877e3af646d34b5110
Your GitHub Username <Your GitHub Email>
2025-03-30 10:23:37 +0530

    checking

--- server/server.js
+++ server/server.js
@@ -2,22 +2,31 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth-routes/index')
+
 // const e = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-cors({
-    origin : process.env.CLIENT_URL,
+
+
+app.use(cors({
+    origin : process.env.CLIENT_URL ,
     methods :["GET","POST","DELETE","PUT"],
     allowedHeaders : ["Content-Type","Authorization"],
-})
+}));
 
 app.use(express.json());
+console.log(MONGO_URI)
+//database
 mongoose
     .connect(MONGO_URI)
     .then(()=>console.log('mongoDB is connected'))
     .catch((e)=>console.log(e));
+//routes config
+
+app.use("/auth",authRoutes);
 
 app.use((err,req,res,next)=>{
     console.log(err.stack);
